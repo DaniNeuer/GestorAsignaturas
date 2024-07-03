0f451b76378c592762286ff11a5736afe8ab9d51
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-03 08:59:41 -0500

    Avance hasta el 1.5 h

---
+++ GestorAsignaturas/Controllers/AsignaturaController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
+using GestorAsignaturas.DAL;
+using GestorAsignaturas.Models;
+namespace GestorAsignaturas.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        private GestorData bd = new GestorData();
+        // GET: Asignatura
+        public ActionResult Index()
+        {
+            return View(bd.Asignaturas.ToList());
+        }
+        //GET: Asignatura/Detalles/5
+        public ActionResult Detalles(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = bd.Asignaturas.Find(id);
+            if(asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+        //GET: Asignatura/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        //POST: Asignatura/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Crear([Bind(Include ="ID,Nombre,Codigo,Creditos,Horas")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                bd.Asignaturas.Add(asignatura);
+                bd.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asignatura);
+        }
+        //GET: Asignatura/Editar/5
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = bd.Asignaturas.Find(id);
+            if(asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+        //POsT: Asignatura/Editar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar([Bind(Include ="ID,nombre,Codigo,Creditos,Horas")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                bd.Entry(asignatura).State=EntityState.Modified;
+                bd.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asignatura);
+        }
+    }
+}
---
+++ GestorAsignaturas/Models/Asignatura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace GestorAsignaturas.Models
+{
+    public class Asignatura
+    {
+        [Key] public int  ID { get; set;  }
+
+        [Required(ErrorMessage ="El nombre de la asignatura es obligatorio.")]
+        [StringLength(100,ErrorMessage ="El nombre de la asignatura no puede superar los 100 caracteres.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El codigo de la asignatura es obligatorio.")]
+        [StringLength(7, ErrorMessage = "El codigo de la asignatura no puede superar los 7 caracteres.")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "El numero de creditos de la asignatura es obligatorio.")]
+        [Range(0,15, ErrorMessage = "El numero de creditos debe estar entre 0 y 15.")]
+        public int Creditos { get; set; }
+
+        [Required(ErrorMessage = "El numero de horas de la asignatura es obligatorio.")]
+        [Range(1, 45, ErrorMessage = "El numero de horas debe estar entre 1 y 45.")]
+        public int Horas { get; set; }
+
+
+
+    }
+}
