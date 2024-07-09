6665280be6546ec89e10cc9c4e76206555af2579
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-09 17:45:21 -0500

    Conclusiones y recomendaciones y el codigo comentado

--- GestorAsignaturas/Controllers/AsignaturaController.cs
+++ GestorAsignaturas/Controllers/AsignaturaController.cs
@@ -15,6 +15,7 @@ namespace GestorAsignaturas.Controllers
         // GET: Asignatura
         public ActionResult Index()
         {
+            // La accion index mandara las asignatorias enlistadas por el metodo ToList()
             return View(bd.Asignaturas.ToList());
         }
         //GET: Asignatura/Detalles/5
@@ -22,10 +23,13 @@ namespace GestorAsignaturas.Controllers
         {
             if(id == null)
             {
+                // Si id es nulo, retorna un código de estado HTTP BadRequest (400)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            // creamos un objeto del modelo asignatura y en ella almacenamos a la asignatura que se busca por el Find(id)
             Asignatura asignatura = bd.Asignaturas.Find(id);
-            if(asignatura == null)
+            // Si la asignatura no existe, retorna un código de estado HTTP NotFound (404)
+            if (asignatura == null)
             {
                 return HttpNotFound();
             }
@@ -36,30 +40,41 @@ namespace GestorAsignaturas.Controllers
         {
             return View();
         }
-
+        // sobrecarga usando los adornor [HttpPost] y con el [ValidateAntiForgeryToken] se usa para proteccion contra ataques
         // POST: Asignatura/Crear
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Crear([Bind(Include = "ID,Nombre,Codigo,Creditos,CD,CP,AA,Area")] Asignatura asignatura)
         {
+            // Verifica si el estado del modelo es válido
             if (ModelState.IsValid)
             {
+                // Agrega la nueva asignatura a la base de datos
                 bd.Asignaturas.Add(asignatura);
+                // Guarda los cambios en la base de datos
                 bd.SaveChanges();
+                // Redirige a la acción Index para mostrar la lista de asignaturas
                 return RedirectToAction("Index");
             }
+            // Si el estado del modelo no es válido, retorna la vista con los datos de la asignatura
             return View(asignatura);
         }
         // GET: Asignatura/Editar/5
+        // Acción del controlador para mostrar la vista de edición de una asignatura existente
         public ActionResult Editar(int? id)
         {
+            // Verifica si el parámetro id es nulo
             if (id == null)
             {
+                // Si id es nulo, retorna un código de estado HTTP BadRequest (400)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            // Busca la asignatura en la base de datos usando el id proporcionado
             Asignatura asignatura = bd.Asignaturas.Find(id);
+            // Verifica si la asignatura existe en la base de datos
             if (asignatura == null)
             {
+                // Si la asignatura no existe, retorna un código de estado HTTP NotFound (404)
                 return HttpNotFound();
             }
             return View(asignatura);
@@ -70,22 +85,31 @@ namespace GestorAsignaturas.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Editar([Bind(Include = "ID,Nombre,Codigo,Creditos,CD,CP,AA,Area")] Asignatura asignatura)
         {
+            // Verifica si el estado del modelo es válido
             if (ModelState.IsValid)
             {
+                // Marca la entidad Asignatura como modificada en el contexto de datos
                 bd.Entry(asignatura).State = EntityState.Modified;
+                // Guarda los cambios en la base de datos
                 bd.SaveChanges();
+                // Redirige a la acción Index para mostrar la lista de asignaturas
                 return RedirectToAction("Index");
             }
+            // Si el estado del modelo no es válido, retorna la vista con los datos de la asignatura
             return View(asignatura);
         }
 
         //GET: Asignatura/Borrar/5
+        // Acción del controlador para mostrar la vista de confirmación de borrado de una asignatura
         public ActionResult Borrar(int? id)
         {
+            // Verifica si el parámetro id es nulo
             if (id == null)
             {
+                // Si id es nulo, retorna un código de estado HTTP BadRequest (400)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            // Busca la asignatura en la base de datos usando el id proporcionado
             Asignatura asignatura = bd.Asignaturas.Find(id);
             if(asignatura == null)
             {
@@ -94,18 +118,29 @@ namespace GestorAsignaturas.Controllers
             return View(asignatura);
         }
         // POST: Asignatura/Borrar/5
+        // Acción del controlador para manejar la confirmación del borrado de una asignatura
         [HttpPost, ActionName("Borrar")]
         [ValidateAntiForgeryToken]
         public ActionResult ConfirmarBorrar(int id)
         {
+            // Busca la asignatura en la base de datos usando el id proporcionado
             Asignatura asignatura = bd.Asignaturas.Find(id);
+
+            // Remueve la asignatura de la base de datos
             bd.Asignaturas.Remove(asignatura);
+
+            // Guarda los cambios en la base de datos
             bd.SaveChanges();
+
+            // Redirige a la acción Index para mostrar la lista de asignaturas
             return RedirectToAction("Index");
         }
+
+        // Método para liberar los recursos utilizados por el contexto de datos
         protected override void Dispose(bool disposing)
         {
-            if(disposing)
+            // Si disposing es true, libera los recursos del contexto de datos
+            if (disposing)
             {
                 bd.Dispose();
             }
--- GestorAsignaturas/Models/Asignatura.cs
+++ GestorAsignaturas/Models/Asignatura.cs
@@ -14,7 +14,17 @@
 // existe en la base de datos y no sera posible encontrar.
 // *
 // Conclusiones:
+// *  La separación de preocupaciones implementada a través del patrón MVC  permite que
+// cada componente de la aplicación (modelos, vistas y controladores) sea desarrollado y mantenido de manera
+// independiente. Esto facilita el mantenimiento del código, tambien permite el reuso de código y la escabilidad de la
+// aplicación.
+// * El uso de validaciones en los modelos garantiza que los datos ingresados cumplan con los requisistos establecidos
+// antes de pasarlos a la base de datos.
+// * El uso de entity Framework para la conexión a la base de datos  permite una integración eficiente entre el modelo de
+// datos y la lógica de negocio, reduciendo la cantidad de código necesario para realizar operaciones CRUD.
 // Recomendaciones:
+// *Es recomendable mejorar las validaciones de los modelos para cubrir todos los posibles casos de uso y garantizar
+// que los datos ingresados sean consistentes y válidos. 
 // ************************************************************************
 using System;
 using System.Collections.Generic;
@@ -22,34 +32,38 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
 
 
 namespace GestorAsignaturas.Models
 {
     public class Asignatura
     {
+        // El adorno [key] indica que sera una llave primaria
         [Key]
         public int ID { get; set; }
-
+        // El adornor indica que el atrinuto nombre es obligatorio y se mostrara el mensaje de error
+        // con el adornor [StringLength]  se indicara el maximo de caracteres que tendra el atributo
         [Required(ErrorMessage = "El nombre de la asignatura es obligatorio.")]
         [StringLength(100, ErrorMessage = "El nombre de la asignatura no puede superar los 100 caracteres.")]
         public string Nombre { get; set; }
-
+        // se indica que el atributo codigo es obligatorio y con un maximo de 7 caracteres
         [Required(ErrorMessage = "El código de la asignatura es obligatorio.")]
         [StringLength(7, ErrorMessage = "El código de la asignatura no puede superar los 7 caracteres.")]
         public string Codigo { get; set; }
-
+        // Se indica que el atributo credito es obligatorio y debe ser un valor entre 0 y 15
         [Required(ErrorMessage = "El número de créditos de la asignatura es obligatorio.")]
         [Range(0, 15, ErrorMessage = "El número de créditos debe estar entre 0 y 15.")]
         public int Creditos { get; set; }
-
+        // Se indica que el atributo CD es obligatorio y su valor debe ser entre 1 y 45
         [Required(ErrorMessage = "El número de horas de clase directa (CD) es obligatorio.")]
         [Range(1, 45, ErrorMessage = "El número de horas de clase directa debe estar entre 1 y 45.")]
         public int CD { get; set; } = 1;
-
+        // Se indica que el atributo CP es obligatorio y su valor debe ser entre 0 y 45
         [Range(0, 45, ErrorMessage = "El número de horas de clase práctica debe estar entre 0 y 45.")]
         public int CP { get; set; }
-
+        //Se indica que el atributo AA es obligatorio y su valor debe ser entre 1 y 45
         [Required(ErrorMessage = "El número de horas de aprendizaje autónomo (AA) es obligatorio.")]
         [Range(1, 45, ErrorMessage = "El número de horas de aprendizaje autónomo debe estar entre 1 y 45.")]
         public int AA { get; set; } = 1;
